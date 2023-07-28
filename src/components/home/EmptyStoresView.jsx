c5713d5fab5908890bddacce92e361ffb53e4ea5
Joel <ndohjoelmbj16@gmail.com>
2023-07-28 20:38:42 +0100

    feat: error check on the store info form

--- src/components/home/EmptyStoresView.jsx
+++ src/components/home/EmptyStoresView.jsx
@@ -6,7 +6,7 @@ function EmptyStoresView() {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-4 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center flex-col cursor-pointer">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex justify-center items-center flex-col">
               <PlusCircleIcon className="text-gray-100 h-48 w-48" />
               <h3 className="text-gray-100 text-xl font-bold">
                 Create a store
