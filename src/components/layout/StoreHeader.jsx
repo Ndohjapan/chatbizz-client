8f2359079cdb34f8e2e7217afe20ec0529f09e61
Joel <ndohjoelmbj16@gmail.com>
2023-07-30 14:30:10 +0100

    fix: I have changed the store dashboard

--- src/components/layout/StoreHeader.jsx
+++ src/components/layout/StoreHeader.jsx
@@ -1,7 +1,6 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
-import { BellIcon, MenuAlt2Icon } from "@heroicons/react/outline";
-import { SearchIcon } from "@heroicons/react/solid";
+import { MenuIcon } from "@heroicons/react/outline";
 
 const userNavigation = [
   { name: "Your Profile", href: "#" },
@@ -17,28 +16,19 @@ function classNames(...classes) {
 function StoreHeader({ setSidebarOpen }) {
   return (
     <>
-      <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 bg-white shadow">
-        <button
-          type="button"
-          className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
-          onClick={() => setSidebarOpen(true)}
-        >
-          <span className="sr-only">Open sidebar</span>
-          <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
-        </button>
-        <div className="flex-1 px-4 flex justify-between">
-          <div className="flex-1 flex items-center">
-            <h1 className="text-3xl font-medium">Lavent Living</h1>
-          </div>
+      <div className="sticky top-0 z-10 flex-shrink-0 flex h-16 justify-between md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-800">
+        <div className="flex items-center">
+          <button
+            type="button"
+            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuIcon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="px-4 flex justify-between">
           <div className="ml-4 flex items-center md:ml-6">
-            <button
-              type="button"
-              className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              <span className="sr-only">View notifications</span>
-              <BellIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-
             {/* Profile dropdown */}
             <Menu as="div" className="ml-3 relative">
               <div>
--- src/components/layout/StoreSideBar.jsx
+++ src/components/layout/StoreSideBar.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Dialog, Transition } from "@headlessui/react";
+import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -19,6 +19,12 @@ const navigation = [
   { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
   { name: "Documents", href: "#", icon: InboxIcon, current: false },
   { name: "Reports", href: "#", icon: ChartBarIcon, current: false },
+];
+
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
 ];
 
 function classNames(...classes) {
@@ -55,7 +61,7 @@ function StoreSideBar({ sidebarOpen, setSidebarOpen }) {
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
           >
-            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800">
+            <div className="relative flex-1 flex flex-col max-w-xs w-full bg-gray-800">
               <Transition.Child
                 as={Fragment}
                 enter="ease-in-out duration-300"
@@ -76,18 +82,18 @@ function StoreSideBar({ sidebarOpen, setSidebarOpen }) {
                   </button>
                 </div>
               </Transition.Child>
-              <div className="flex-shrink-0 flex items-center px-4">
-                <Link to="/">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
-                </Link>
-              </div>
-              <div className="mt-5 flex-1 h-0 overflow-y-auto">
+              <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+                <div className="flex-shrink-0 flex items-center px-4">
+                  <Link to="/">
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                      alt="Workflow"
+                    />
+                  </Link>
+                </div>
                 <StoreList />
-                <nav className="px-2 space-y-1">
+                <nav className="mt-5 px-2 space-y-1">
                   {navigation.map((item) => (
                     <a
                       key={item.name}
@@ -115,8 +121,8 @@ function StoreSideBar({ sidebarOpen, setSidebarOpen }) {
               </div>
             </div>
           </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
+          <div className="flex-shrink-0 w-14">
+            {/* Force sidebar to shrink to fit close icon */}
           </div>
         </Dialog>
       </Transition.Root>
@@ -125,18 +131,18 @@ function StoreSideBar({ sidebarOpen, setSidebarOpen }) {
       <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex-1 flex flex-col min-h-0 bg-gray-800">
-          <div className="flex items-center h-16 flex-shrink-0 px-4 bg-gray-900">
-            <Link to="/">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                alt="Workflow"
-              />
-            </Link>
-          </div>
-          <div className="flex-1 flex flex-col overflow-y-auto">
+          <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4">
+              <Link to="/">
+                <img
+                  className="h-8 w-auto"
+                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                  alt="Workflow"
+                />
+              </Link>
+            </div>
             <StoreList />
-            <nav className="flex-1 px-2 py-4 space-y-1">
+            <nav className="mt-5 flex-1 px-2 space-y-1">
               {navigation.map((item) => (
                 <a
                   key={item.name}
@@ -161,6 +167,63 @@ function StoreSideBar({ sidebarOpen, setSidebarOpen }) {
                 </a>
               ))}
             </nav>
+          </div>
+          <div className="flex items-center">
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative w-full">
+              <div>
+                <Menu.Button className="max-w-xs bg-white flex items-center text-sm focus:outline-none w-full">
+                  <div className="hidden flex-shrink-0 md:flex bg-gray-700 p-4 w-full">
+                    <a href="#" className="flex-shrink-0 w-full group block">
+                      <div className="flex items-center">
+                        <div>
+                          <img
+                            className="inline-block h-9 w-9 rounded-full"
+                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            alt=""
+                          />
+                        </div>
+                        <div className="ml-3">
+                          <p className="text-sm font-medium text-white">
+                            Tom Cook
+                          </p>
+                          <p className="text-xs font-medium text-gray-300 group-hover:text-gray-200">
+                            View profile
+                          </p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="origin-top-right absolute right-4 -top-32 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  {userNavigation.map((item) => (
+                    <Menu.Item key={item.name}>
+                      {({ active }) => (
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm text-gray-700"
+                          )}
+                        >
+                          {item.name}
+                        </a>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </div>
       </div>
