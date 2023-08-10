d0c11236ea81186cda2e6e51956104db271621e5
Joel <ndohjoelmbj16@gmail.com>
2023-08-10 13:32:07 +0100

    chore: completed the conversation page

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -1,42 +1,7 @@
-import ImagesRender from "./ImagesRender";
-import BasicInfo from "./BasicInfo";
+import React from 'react'
 
-
-function Example() {
+export default function Example() {
   return (
-    <div className="bg-white">
-      <div className="max-w-2xl mx-auto pb-16 pt-6 px-4 sm:pb-24 sm:pt-8 sm:px-6 lg:max-w-7xl lg:px-8">
-        {/* Product header */}
-        <div className="pb-6 sm:pb-8 flex items-center justify-between">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
-            Product
-          </h3>
-          <div className="mt-3 flex sm:mt-0 sm:ml-4">
-            <button
-              type="button"
-              className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Discard
-            </button>
-            <button
-              type="button"
-              className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-        <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
-          {/* Image gallery */}
-          <ImagesRender />
-
-          {/* Product info */}
-          <BasicInfo/>
-          
-        </div>
-      </div>
-    </div>
-  );
+    <div>Example</div>
+  )
 }
-
-export default Example;
